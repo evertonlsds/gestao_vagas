210d517779b8fd4e442126fd0e119c0b000e6761
evertonlsds <everton0102@gmail.com>
2024-05-23 18:40:08 -0300

    config controller

---
+++ src/main/java/br/com/evertonsantos/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -0,0 +1,8 @@
+package br.com.evertonsantos.gestao_vagas.modules.candidate;
+
+public class CandidateEntity {
+
+    private String name;
+    private String username;
+    
+}
---
+++ src/main/java/br/com/evertonsantos/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,12 @@
+package br.com.evertonsantos.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    
+    
+}
