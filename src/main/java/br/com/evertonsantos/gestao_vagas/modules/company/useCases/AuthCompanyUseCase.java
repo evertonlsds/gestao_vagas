a636bbb97cdd1ea7f1e16cdcd4226d8ae252be9f
evertonlsds <everton0102@gmail.com>
2024-06-11 13:48:05 -0300

    tested the authentication and obtained the generated token

--- src/main/java/br/com/evertonsantos/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/evertonsantos/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -3,9 +3,13 @@ package br.com.evertonsantos.gestao_vagas.modules.company.useCases;
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 
 import br.com.evertonsantos.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import br.com.evertonsantos.gestao_vagas.modules.company.repositories.CompanyRepository;
@@ -13,23 +17,35 @@ import br.com.evertonsantos.gestao_vagas.modules.company.repositories.CompanyRep
 @Service
 public class AuthCompanyUseCase {
 
+    @Value("${security.token.secret}")
+    private String secretKey;
+
     @Autowired
     private CompanyRepository companyRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public void execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
         var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
                 () -> {
                     throw new UsernameNotFoundException("Company not found");
                 });
-
+        // Vericar a senha são iguais
         var passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
 
-        if(!passwordMatches){
+        // se não for igual -> erro
+        if (!passwordMatches) {
             throw new AuthenticationException();
         }
+
+        // se for igual -> gerar o token
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+       var token = JWT.create().withIssuer("javagas")
+        .withSubject(company.getId().toString())
+        .sign(algorithm);
+        return token;
     }
 
 }
